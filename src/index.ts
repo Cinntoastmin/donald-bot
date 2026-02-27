17c7fe4b7954728b66586d6fc20b110f4db47c71
Tucker Noniewicz <tnoniewicz@comcast.net>
2026-02-27 13:26:29 -0500

    actually made this do the start of what I want it to do

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,64 @@
-import { Client } from 'discord.js';
-import { config } from './config';
-import { commands } from './commands';
-import { deployCommands } from './deploy_commands';
+import { Client, GatewayIntentBits, GuildMember } from 'discord.js';
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const client = new Client({
-    intents:["Guilds", "GuildMessages", "DirectMessages"],
+    intents:[
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
+    ],
 });
 
 client.once("clientReady", () => {
-    console.log("BONESAWS READY");
+    console.log(`Logged in as ${client.user?.tag}`);
+    startMsgTimer();
 });
 
-client.on("guildCreate", async (guild) => {
-    await deployCommands({ guildId: guild.id });
-});
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+    console.log(`[${message.guild?.name}] ${message.author.username}: ${message.content}`);
+})
 
-client.on("interactionCreate", async (interaction) => {
-    console.log("an interaction finally!")
-    if(!interaction.isCommand()) {
+async function decideTargetAndDm(){
+    const guild = await client.guilds.fetch(process.env.GUILD_ID!);
+    const members = await guild.members.fetch();
+    const humanMembers = members.filter((m:GuildMember) => !m.user.bot);
+
+    if(humanMembers.size === 0){
+        console.log("No one is home!");
         return;
     }
-    const { commandName } = interaction;
-    if (commands[commandName as keyof typeof commands]) {
-        commands[commandName as keyof typeof commands].execute(interaction);
-    }
-});
 
-client.login(config.DISCORD_TOKEN)
+    const randomIndex = Math.floor(Math.random() * humanMembers.size);
+    const randomMember = humanMembers.at(randomIndex)!;
+    const messages =[
+        "test 1",
+        "test 2",
+        "test 3",
+    ];
+
+    const randMsg = messages[Math.floor(Math.random() * messages.length)];
+
+    try {
+        await randomMember.send(randMsg);
+        console.log(`Sent ${randMsg} to ${randomMember.user.username}`);
+    } catch (err){
+        console.log(`couldn't dm ${randomMember.user.username}`);
+    }
+}
+
+function startMsgTimer(){
+    const interval = 60*60*1000;
+
+    setInterval(() => {
+        decideTargetAndDm();
+    }, interval);
+
+    setTimeout(() => decideTargetAndDm(), 5000);
+}
+
+client.login(process.env.BOT_TOKEN)
